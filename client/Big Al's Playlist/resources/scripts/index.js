f1069f8a6cbc9c7f55d1a9052c0a648f02d2888a
Rywals <rrnichols@crimson.ua.edu>
2022-04-14 19:42:02 -0500

    final

--- client/Big Al's Playlist/resources/scripts/index.js
+++ client/Big Al's Playlist/resources/scripts/index.js
@@ -1,27 +1,33 @@
-var baseurl = "https://localhost:3001/api/Songs";
+var baseurl = "https://localhost:5401/Songs";
+var sendSong = {};
 
+function handleOnLoad(){
+    populateList();
+}
 
+function populateList(){
 
-function findSongs(){
-    var url = "https://www.songsterr.com/a/ra/songs.json?pattern="
-    let searchString = document.getElementById("searchSong").value;
+    console.log(baseurl)
 
-    url += searchString;
-
-    console.log(searchString)
-
-    fetch(url).then(function(response) {
+    fetch(baseurl).then(function(response) {
 		console.log(response);
 		return response.json();
 	}).then(function(json) {
         console.log(json)
         let html = ``;
 		json.forEach((song) => {
-            console.log(song.title)
+            html += `<div id = "cards" class = "container">`;
+            html += `<div class = "row">`;
             html += `<div class="card col-md-4 bg-dark text-white">`;
 			html += `<img src="./resources/images/music.jpeg" class="card-img" alt="...">`;
 			html += `<div class="card-img-overlay">`;
-			html += `<h5 class="card-title">`+song.title+`</h5>`;
+			html += `<h5 class="card-title">`+song.songTitle+`</h5>`;
+            html += `<div id = "buttonDelete" class = "button">`;
+            html += `<button class="btn btn-dark" onclick="deleteSong(`+song.songID+`)">Delete Song</button>`;
+            html += `<div id = "buttonFavorite" class = "button">`;
+            html += `<button class="btn btn-dark" onclick="putSong(`+song.songID + `,`+ song.favorited+ `)">Favorite</button>`;
+            html += `</div>`;
+            html += `</div>`;
             html += `</div>`;
             html += `</div>`;
 		});
@@ -29,9 +35,73 @@ function findSongs(){
         if(html === ``){
             html = "No Songs found :("
         }
-		document.getElementById("searchSongs").innerHTML = html;
+		document.getElementById("cards").innerHTML = html;
 
 	}).catch(function(error) {
 		console.log(error);
 	})
 }
+
+function putSong(id, favorited){
+    var putAPISongUrl = baseurl + "/" + id+ "/" + favorited;
+    console.log(id);
+    console.log(favorited);
+    sendSong = {
+        id: id,
+        songTimestamp: "2022-04-13T03:59:15",
+        fav: favorited
+    }
+    fetch(putAPISongUrl,{
+        method: "PUT",
+        headers: {
+            "Accept": 'application/json',
+            "Content-Type": 'application/json',
+        },
+        body: JSON.stringify(sendSong)
+    })
+    .then((response) => {
+        populateList();
+    })
+}
+
+function postSong(){
+    var postAPISongUrl = baseurl;
+    sendSong = {
+        songTitle: document.getElementById("title").value,
+        songTimestamp: "2022-04-13T03:59:15",
+        deleted:"test"
+    }
+    fetch(postAPISongUrl,{
+        method: "POST",
+        headers: {
+            "Accept": 'application/json',
+            "Content-Type": 'application/json',
+        },
+        body: JSON.stringify(sendSong)
+    })
+    .then((response) => {
+        populateList();
+    })
+}
+
+
+function deleteSong(id){
+    var deleteAPISongUrl = baseurl + "/"+ id;
+    sendSong = {
+        id: id,
+        songTimestamp: "2022-04-13T03:59:15",
+        deleted:"test"
+    }
+    fetch(deleteAPISongUrl,{
+        method: "DELETE",
+        headers: {
+            "Accept": 'application/json',
+            "Content-Type": 'application/json',
+        },
+        body: JSON.stringify(sendSong)
+    })
+    .then((response) => {
+        populateList();
+    })
+}
+
